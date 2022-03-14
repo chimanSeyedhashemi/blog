597bc93f16d56fd102eb9e4f7349d612745d84b6
Chiman Seyedhashemi <ch.seyedhashemi@gmail.com>
2022-03-14 21:05:50 +0330

    Add documnet

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+import React from "react";
+
+export default class AppDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+AppDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => <App {...props} />,
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+  };
+};
